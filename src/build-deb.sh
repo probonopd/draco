3237fa39300fbb563c334c437bf25a5a9dfc3fea
probonopd <probonopd@users.noreply.github.com>
2020-05-09 11:51:38 +0000

    Update build-deb.sh

--- src/build-deb.sh
+++ src/build-deb.sh
@@ -6,12 +6,25 @@
 
 sudo rm -rf debian/ || true
 
-# -DCMAKE_INSTALL_LIBDIR="lib/x86_64-linux-gnu"
-cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_INSTALL_LIBEXECDIR=bin -DCMAKE_BUILD_TYPE=Release ..
+# FIXME: Why do we need to specify -DCMAKE_INSTALL_LIBDIR="lib/x86_64-linux-gnu"?
+cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_INSTALL_LIBDIR="lib/x86_64-linux-gnu" -DCMAKE_INSTALL_LIBEXECDIR=bin -DCMAKE_BUILD_TYPE=Release ..
 make -j ${nproc}
 
 PROGRAMNAME=draco
+
 mkdir -v -p debian/DEBIAN
+echo "Package: ${PROGRAMNAME}
+Version: ${VERSION}
+Priority: optional
+Architecture: ${ARCH}
+Depends: libc, bash (>=4.0)
+Installed-Size: ${INSTALLED_SIZE}
+Maintainer: No Name <noname@gmail.com>
+Description: Draco is a simple and lightweight desktop environment.
+ While small still features XDG integration, freedesktop services and integration,
+ power and storage management, desktop, panels, multi-monitor support and much more.
+ Draco does not include any user applications." > debian/DEBIAN/control
+
 sudo make DESTDIR=debian install
 
 # Only build deb for one of the builds
@@ -33,18 +46,6 @@ chmod 0644 debian/usr/share/doc/${PROGRAMNAME}/changelog.gz
 
 INSTALLED_SIZE=$(du -s debian/usr | awk '{x=$1/1024; i=int(x); if ((x-i)*10 >= 5) {f=1} else {f=0}; print i+f}')
 echo "size=${INSTALLED_SIZE}"
-
-echo "Package: ${PROGRAMNAME}
-Version: ${VERSION}
-Priority: optional
-Architecture: ${ARCH}
-Depends: libc, bash (>=4.0)
-Installed-Size: ${INSTALLED_SIZE}
-Maintainer: No Name <noname@gmail.com>
-Description: Draco is a simple and lightweight desktop environment.
- While small still features XDG integration, freedesktop services and integration,
- power and storage management, desktop, panels, multi-monitor support and much more.
- Draco does not include any user applications." > debian/DEBIAN/control
 
 fakeroot dpkg-deb --build debian
 
