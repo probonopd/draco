efcc90d343fb3752565937027c08df0a730de47e
probonopd <probonopd@users.noreply.github.com>
2020-05-09 12:33:02 +0000

    Update build-deb.sh

--- src/build-deb.sh
+++ src/build-deb.sh
@@ -34,7 +34,7 @@ echo "Package: ${PROGRAMNAME}
 Version: ${VERSION}
 Priority: optional
 Architecture: ${ARCH}
-Depends: libc, bash (>=4.0), qterminal, pnmixer, openbox, hicolor-icon-theme, xscreensaver, xdg-utils, openbox, udisks2, upower
+Depends: bash (>=4.0), qterminal, pnmixer, openbox, hicolor-icon-theme, xscreensaver, xdg-utils, openbox, udisks2, upower
 Installed-Size: ${INSTALLED_SIZE}
 Maintainer: No Name <noname@gmail.com>
 Description: Draco is a simple and lightweight desktop environment.
